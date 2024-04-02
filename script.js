573fd7efb3b49a7af37871a705507e8e068297af
Leca46 <jereerkkila95@gmail.com>
2024-04-02 10:13:08 +0300

    Finish the console versio of the game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const getComputerChoice = () => {
+const getcomputerSelection = () => {
     // Create an array of objects that the 'computer' uses
     const choices = ['rock', 'paper', 'scissors']
 
@@ -6,23 +6,39 @@ const getComputerChoice = () => {
     const randomIndex = Math.floor(Math.random() * choices.length)
 
     // Use the random index to select a random element from the choices array
-    const computerChoice = choices[randomIndex]
+    const computerSelection = choices[randomIndex]
 
     // Return the computer's choice
-    return computerChoice
+    return computerSelection
 }
-console.log(getComputerChoice())
+console.log(getcomputerSelection())
 
 const playerSelection = prompt();
-const computerSelection = getComputerChoice();
+const computerSelection = getcomputerSelection();
 
 const playRound = (playerSelection, computerSelection) => {
-    if (playerSelection === computerSelection) {
+    if ((playerSelection === computerSelection) && (computerSelection === playerSelection)) {
         console.log("It's a tie!");
     }
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        console.log('You win! Rock beats  scissors');
+    else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
+        return "You Win! Scissors beats paper!";
     }
+    else if ((playerSelection === "scissors") && (computerSelection === "rock")) {
+        return "You Lose! Rock beats scissors!";
+    } 
+    else if ((playerSelection === "rock") && (computerSelection === "paper")) {
+        return "You Lose! Paper beats rock!";
+    } 
+    else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+        return "You Win! Rock beats scissors!";
+    } 
+    else if ((playerSelection === "paper") && (computerSelection === "rock")) {
+        return "You Win! Paper beats rock!";
+    } 
+    else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
+        return "You Lose! Scissors beats paper!";
+    }  
+ 
     return `You played ${playerSelection} and the computer played ${computerSelection}.`;
 }
 console.log(playRound(playerSelection, computerSelection));
